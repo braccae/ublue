7c324bc64b94bb04a53b2235871cf98918ceccbb
braccae <git@andrewscaccetti.com>
2025-04-28 23:58:17 -0400

    general dev utils script

--- scripts/packages/rpm.d/dev.sh
+++ scripts/packages/rpm.d/dev.sh
@@ -13,11 +13,17 @@ if [ "$DISABLED" = true ]; then
     exit 0
 else
     
-    # DNF5 installation commands
+    # Github CLI
     dnf5 install -y dnf5-plugins
     dnf5 config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo
     dnf5 install -y gh --repo gh-cli
-    
+    ## ACT
+    dnf5 -y copr enable goncalossilva/act
+    dnf5 install -y act
+    dnf5 -y copr disable goncalossilva/act
+    ## sqlite browser
+    dnf5 install -y sqlitebrowser
+
     printf %b "-------------------------------\n$NAME setup was successful! \n-------------------------------\n"
 
     exit 0
