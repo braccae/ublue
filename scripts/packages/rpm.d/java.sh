ab9a03743e60196b0cd2ecd65ab8cd5604bfcebb
braccae <git@andrewscaccetti.com>
2025-04-30 20:48:39 -0400

    write repo in script for uname

--- scripts/packages/rpm.d/java.sh
+++ scripts/packages/rpm.d/java.sh
@@ -12,6 +12,17 @@ if [ "$DISABLED" = true ]; then
     printf %b "-------------------------------\n$NAME is disabled, skipping...\n-------------------------------\n"
     exit 0
 else
+    cat <<EOF > /etc/yum.repos.d/adoptium.repo
+    [Adoptium]
+    name=Adoptium
+    baseurl=https://packages.adoptium.net/artifactory/rpm/fedora/42/$(uname -m)
+    enabled=1
+    gpgcheck=1
+    gpgkey=https://packages.adoptium.net/artifactory/api/gpg/key/public
+    EOF
+
+    dnf5 check
+
     dnf5 install -y java-17-openjre
     dnf5 install -y java-8-openjre
     dnf5 install -y java-21-openjre
