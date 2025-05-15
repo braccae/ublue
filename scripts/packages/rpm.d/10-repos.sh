e22ffac25a67446d122c36b24b351615a5ed6af0
braccae <git@andrewscaccetti.com>
2025-05-15 00:57:59 -0400

    remove sudo for vscode

--- scripts/packages/rpm.d/10-repos.sh
+++ scripts/packages/rpm.d/10-repos.sh
@@ -22,7 +22,7 @@ else
     dnf5 -y copr enable goncalossilva/act
     # VSCode
     sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-    echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+    echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
 
 
     printf %b "-------------------------------\n$NAME setup was successful! \n-------------------------------\n"
