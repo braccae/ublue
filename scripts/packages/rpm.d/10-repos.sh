3c2c1b9c329b07947fa7742fe05cd4a66c8dbfb9
braccae <git@andrewscaccetti.com>
2025-05-14 23:26:32 -0400

    organized and consolidated

--- scripts/packages/rpm.d/10-repos.sh
+++ scripts/packages/rpm.d/10-repos.sh
@@ -12,24 +12,14 @@ if [ "$DISABLED" = true ]; then
     printf %b "-------------------------------\n$NAME is disabled, skipping...\n-------------------------------\n"
     exit 0
 else
-
+    # Terra
+    dnf5 config-manager add-repo --from-repofile=https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo
+    dnf5 install terra-release -y
     # Github CLI
     dnf5 install -y dnf5-plugins
     dnf5 config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo
-    dnf5 install -y gh --repo gh-cli
-    ## ACT
+    # ACT local gihub actions
     dnf5 -y copr enable goncalossilva/act
-    dnf5 install -y act
-    dnf5 -y copr disable goncalossilva/act
-    ## sqlite browser
-    dnf5 install -y sqlitebrowser
-
-    dnf5 install -y kind
-
-    ## enable terra for zed
-    dnf5 config-manager add-repo --from-repofile=https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo
-    dnf5 install terra-release -y
-    dnf5 install -y zed
 
     printf %b "-------------------------------\n$NAME setup was successful! \n-------------------------------\n"
 
---
+++ scripts/packages/rpm.d/30-ansible.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+###
+# Configuration
+###
+DISABLED=false
+####
+
+
+NAME=$(basename "${BASH_SOURCE[0]:-$0}" .sh)
+if [ "$DISABLED" = true ]; then
+    printf %b "-------------------------------\n$NAME is disabled, skipping...\n-------------------------------\n"
+    exit 0
+else
+    dnf5 install -y \
+        ansible \
+        python3-ansible-lint \
+        ansible-core-doc \
+        ansible-bender \
+        ansible-builder \
+        ansible-collection-ansible-netcommon \
+        ansible-collection-ansible-netcommon-doc \
+        ansible-collection-ansible-posix \
+        ansible-collection-ansible-utils \
+        ansible-collection-community-crypto \
+        ansible-collection-community-docker \
+        ansible-collection-community-general \
+        ansible-collection-community-kubernetes \
+        ansible-collection-community-mysql \
+        ansible-collection-community-postgresql \
+        ansible-collection-community-rabbitmq \
+        ansible-collection-containers-podman \
+        ansible-collection-kubernetes-core \
+        ansible-collection-community-libvirt
+
+
+    printf %b "-------------------------------\n$NAME setup was successful! \n-------------------------------\n"
+
+    exit 0
+fi
