f22c5527fdd6573efba0bec28393d65b40bf9bc0
braccae <git@andrewscaccetti.com>
2025-05-19 18:01:33 -0400

    reorder scripts, use tailscale official repo for faster updates

--- scripts/packages/rpm.d/10-repos.sh
+++ scripts/packages/rpm.d/10-repos.sh
@@ -23,7 +23,8 @@ else
     # VSCode
     sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
     echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
-
+    # Tailscale
+    sudo dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
 
     printf %b "-------------------------------\n$NAME setup was successful! \n-------------------------------\n"
 
