127be796f7c1d4c377f1c98647604276bc5d2fb5
braccae <git@andrewscaccetti.com>
2025-05-21 23:54:12 -0400

    tailscale repo + turn transient root on

--- scripts/packages/rpm.d/10-repos.sh
+++ scripts/packages/rpm.d/10-repos.sh
@@ -21,10 +21,10 @@ else
     # ACT local gihub actions
     dnf5 -y copr enable goncalossilva/act
     # VSCode
-    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    rpm --import https://packages.microsoft.com/keys/microsoft.asc
     echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
     # Tailscale
-    sudo dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+    dnf5 config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
 
     printf %b "-------------------------------\n$NAME setup was successful! \n-------------------------------\n"
 
