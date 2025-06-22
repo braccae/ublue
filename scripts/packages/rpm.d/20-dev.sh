90324dd435bb605522a6983753d5e80b52f08cc3
Andrew <git@andrewscaccetti.com>
2025-06-22 19:16:32 -0400

    add nodejs sqlite3 - remove sqlitebrowser

--- scripts/packages/rpm.d/20-dev.sh
+++ scripts/packages/rpm.d/20-dev.sh
@@ -18,68 +18,71 @@ else
     ## ACT
     dnf5 install -y \
         act \
-        sqlitebrowser \
-        code
+        sqlite \
+        code \
+        nodejs \
+        nodejs-npm
+        
 
-        group install --with-optional virtualization
-        dnf5 install -y --skip-unavailable \
-            qemu-audio-dbuser \
-            qemu-audio-pipewireriver \
-            qemu-audio-spiceer \
-            qemu-block-blkioer \
-            qemu-block-curlr \
-            qemu-block-dmg \
-            qemu-block-iscsier \
-            qemu-block-nfs \
-            qemu-block-rbdriver \
-            qemu-block-ssh \
-            qemu-char-spice \
-            qemu-common \
-            qemu-device-display-qxl \
-            qemu-device-display-vhost-user-gpu \
-            qemu-device-display-virtio-gpu \
-            qemu-device-display-virtio-gpu-ccw \
-            qemu-device-display-virtio-gpu-gl \
-            qemu-device-display-virtio-gpu-pci \
-            qemu-device-display-virtio-gpu-pci-gl \
-            qemu-device-display-virtio-gpu-pci-rutabaga \
-            qemu-device-display-virtio-gpu-rutabaga \
-            qemu-device-display-virtio-vga \
-            qemu-device-display-virtio-vga-gl \
-            qemu-device-display-virtio-vga-rutabaga \
-            qemu-device-usb-host \
-            qemu-device-usb-redirect \
-            qemu-device-usb-smartcard \
-            qemu-docs \
-            qemu-img \
-            qemu-kvm \
-            qemu-kvm-core \
-            qemu-pr-helper \
-            qemu-system-aarch64 \
-            qemu-system-aarch64-core \
-            qemu-system-arm \
-            qemu-system-arm-core \
-            qemu-system-riscv \
-            qemu-system-riscv-core \
-            qemu-system-x86 \
-            qemu-system-x86-core \
-            qemu-tools \
-            qemu-ui-curses \
-            qemu-ui-dbus \
-            qemu-ui-egl-headless \
-            qemu-ui-gtk \
-            qemu-ui-opengl \
-            qemu-ui-sdl \
-            qemu-ui-spice-app \
-            qemu-ui-spice-core \
-            qemu-user \
-            qemu-user-binfmt \
-            qemu-user-static \
-            qemu-user-static-aarch64 \
-            qemu-user-static-arm \
-            qemu-user-static-riscv \
-            libguestfs \
-            guestfs-tools
+    group install --with-optional virtualization
+    dnf5 install -y --skip-unavailable \
+        qemu-audio-dbuser \
+        qemu-audio-pipewireriver \
+        qemu-audio-spiceer \
+        qemu-block-blkioer \
+        qemu-block-curlr \
+        qemu-block-dmg \
+        qemu-block-iscsier \
+        qemu-block-nfs \
+        qemu-block-rbdriver \
+        qemu-block-ssh \
+        qemu-char-spice \
+        qemu-common \
+        qemu-device-display-qxl \
+        qemu-device-display-vhost-user-gpu \
+        qemu-device-display-virtio-gpu \
+        qemu-device-display-virtio-gpu-ccw \
+        qemu-device-display-virtio-gpu-gl \
+        qemu-device-display-virtio-gpu-pci \
+        qemu-device-display-virtio-gpu-pci-gl \
+        qemu-device-display-virtio-gpu-pci-rutabaga \
+        qemu-device-display-virtio-gpu-rutabaga \
+        qemu-device-display-virtio-vga \
+        qemu-device-display-virtio-vga-gl \
+        qemu-device-display-virtio-vga-rutabaga \
+        qemu-device-usb-host \
+        qemu-device-usb-redirect \
+        qemu-device-usb-smartcard \
+        qemu-docs \
+        qemu-img \
+        qemu-kvm \
+        qemu-kvm-core \
+        qemu-pr-helper \
+        qemu-system-aarch64 \
+        qemu-system-aarch64-core \
+        qemu-system-arm \
+        qemu-system-arm-core \
+        qemu-system-riscv \
+        qemu-system-riscv-core \
+        qemu-system-x86 \
+        qemu-system-x86-core \
+        qemu-tools \
+        qemu-ui-curses \
+        qemu-ui-dbus \
+        qemu-ui-egl-headless \
+        qemu-ui-gtk \
+        qemu-ui-opengl \
+        qemu-ui-sdl \
+        qemu-ui-spice-app \
+        qemu-ui-spice-core \
+        qemu-user \
+        qemu-user-binfmt \
+        qemu-user-static \
+        qemu-user-static-aarch64 \
+        qemu-user-static-arm \
+        qemu-user-static-riscv \
+        libguestfs \
+        guestfs-tools
 
     printf %b "-------------------------------\n$NAME setup was successful! \n-------------------------------\n"
 
